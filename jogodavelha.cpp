3fd250415c0fea8bdaecb70966f97d8230d16daa
Alesson Sousa <alessonsousaviana@gmail.com>
2020-02-05 01:11:37 -0300

    1.0

---
+++ jogodavelha.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+
+int num;
+
+cout << "olá, mundo" <<endl;
+
+cout << "Digite um valor:";
+cin >>num;
+
+cout << "valor:",num;
+
+
+}
